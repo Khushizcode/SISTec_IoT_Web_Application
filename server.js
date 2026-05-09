bfffac9c99e13e972fa0c674d007344efaa9c066
Khushi <misskhushin.02@gmail.com>
2026-05-09 14:43:29 +0530

    fixed home route

--- server.js
+++ server.js
@@ -173,6 +173,10 @@ app.get("/api/lcd", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(PORT, () => {
     console.log("Server Running...");
 });
